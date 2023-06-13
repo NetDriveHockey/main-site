6765920c64e86196544ff93e7a6ab9ff5f78f48e
lukewismer <lukewismer77@gmail.com>
2023-06-13 10:23:42 -0600

    update gitignores

---
+++ website/src/Firebase/Firebase.js
@@ -0,0 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import firebaseConfig from './firebaseConfig.json';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
